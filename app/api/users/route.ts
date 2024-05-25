313c1964a08ec7cde4757ae049b493b0be4816c0
weiduner <84208389+weiduner@users.noreply.github.com>
2024-05-25 05:03:46 -0700

    api user cache

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,7 +1,9 @@
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
+import { unstable_noStore as noStore } from "next/cache";
 
 export async function GET(request: NextRequest) {
+  noStore();
   const users = await prisma.user.findMany({ orderBy: { name: "asc" } });
   return NextResponse.json(users);
 }
