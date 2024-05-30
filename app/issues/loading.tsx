6abfa5f4a849b918be262ce6472435d5db5ecea4
weiduner <84208389+weiduner@users.noreply.github.com>
2024-05-30 11:03:58 -0700

    bug fix

--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -1,4 +1,4 @@
-import { Table } from "@radix-ui/themes";
+import { Flex, Table } from "@radix-ui/themes";
 import React from "react";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
@@ -7,8 +7,8 @@ import IssueActions from "./IssueActions";
 const LoadingIssuesPage = () => {
   const issues = [1, 2, 3, 4, 5];
   return (
-    <div>
-      <IssueActions />
+    <Flex direction="column" gap="3">
+      <IssueActions session={null} />
       <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
@@ -45,7 +45,7 @@ const LoadingIssuesPage = () => {
           ))}
         </Table.Body>
       </Table.Root>
-    </div>
+    </Flex>
   );
 };
 
